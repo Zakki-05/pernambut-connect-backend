cf03694b47b5ef29faf102fdd8b554c4ba3961e8
zakki-05 <zakkiadnan05@gmail.com>
2026-05-14 14:23:44 +0530

    feat: initialize Django project settings with REST framework, JWT authentication, and Firebase integration

--- backend/settings.py
+++ backend/settings.py
@@ -104,7 +104,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'your-email@gmail.com'  # <-- PLEASE PROVIDE YOUR EMAIL
+EMAIL_HOST_USER = 'zakkiadnan05@gmail.com'
 EMAIL_HOST_PASSWORD = 'feoj suud shdh dgsq'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
